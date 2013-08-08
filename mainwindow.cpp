bf2dd29a538748127d582e7e0fecafcd43a065b7
Lander Brandt <landerbrandt@gmail.com>
2013-08-07 21:35:04 -0700

    Added 'General' screen. Made app dynamically resize

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,18 +3,31 @@
 #include <QDebug>
 #include <QDir> // for temp path
 #include <QtMacExtras/QMacNativeToolBar>
+#include <QPropertyAnimation>
+#include <QStatusBar>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    layout()->setSizeConstraint(QLayout::SetFixedSize);
+    //this->setMinimumSize(ui->pageGeneral->sizeHint());
+    //this->setMaximumSize(ui->pageGeneral->sizeHint());
+    ui->pageGeneral->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    ui->pageGeneral->adjustSize();
+    adjustSize();
+    animation = std::unique_ptr<QPropertyAnimation>(new QPropertyAnimation(this, "size"));
+
+    // remove min/maximize buttons and fullscreen buttons
+    this->setWindowFlags((this->windowFlags() & (~(Qt::WindowMinMaxButtonsHint | Qt::WindowFullscreenButtonHint))));
+    ui->stackedWidget->setCurrentIndex(0);
 
     toolBarGroup = std::unique_ptr<QActionGroup>(new QActionGroup(this));
     setToolBarGroup();
     // makes the toolbar look like "not shit" as I'd describe it
     QtMacExtras::setNativeToolBar(ui->toolBar);
-    appSettings = std::shared_ptr<QSettings>(new QSettings("lander", "unnamed"));
+    appSettings = std::shared_ptr<QSettings>(new QSettings(QSettings::NativeFormat, QSettings::UserScope, "lander", "unnamed"));
 
     // Get the credentials from the settings
     appSettings->beginGroup("AWS Credentials");
@@ -23,6 +36,9 @@ MainWindow::MainWindow(QWidget *parent) :
     userCredentials.AccessSecretKey = appSettings->value("Secret", "").toString().toStdString();
     appSettings->endGroup();
 
+    connect(ui->stackedWidget, SIGNAL(currentChanged(int)), this, SLOT(stackedWidgetPageChanged(int)));
+
+    // Toolbar stuff
     ui->lineS3Bucket->setText(userCredentials.BucketName.c_str());
     ui->lineS3Id->setText(userCredentials.AccessKeyId.c_str());
     ui->lineS3Secret->setText(userCredentials.AccessSecretKey.c_str());
@@ -31,14 +47,24 @@ MainWindow::MainWindow(QWidget *parent) :
     connect(ui->lineS3Id, SIGNAL(editingFinished()), this, SLOT(lineIdEditingFinished()));
     connect(ui->lineS3Secret, SIGNAL(editingFinished()), this, SLOT(lineSecretEditingFinished()));
 
+    // Finally initialize the main CaptureUtils instance
     captureUtil = std::unique_ptr<CaptureUtils>(new CaptureUtils(QDir::tempPath().toStdString()));
     captureUtil->AwsCredentials = userCredentials;
     captureUtil->SetupHotkeys();
 }
 
+void MainWindow::resizeWithAnimation( const QSize& size)
+{
+    animation->setDuration(150);
+    animation->setStartValue(this->size());
+    animation->setEndValue(size);
+    animation->start();
+}
+
 void MainWindow::setToolBarGroup( void )
 {
-    ui->actionS3->setChecked(true);
+    ui->actionGeneral_Settings->setChecked(true);
+    toolBarGroup->addAction(ui->actionGeneral_Settings);
     toolBarGroup->addAction(ui->actionS3);
     toolBarGroup->addAction(ui->actionAbout);
 
@@ -47,18 +73,44 @@ void MainWindow::setToolBarGroup( void )
     spacer->setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
     ui->toolBar->insertWidget(ui->actionAbout, spacer);
 
-    connect(ui->actionS3, SIGNAL(triggered(bool)), this, SLOT(toolBarS3Clicked(bool)));
-    connect(ui->actionAbout, SIGNAL(triggered(bool)), this, SLOT(toolBarAboutClicked(bool)));
+    connect(ui->actionGeneral_Settings, SIGNAL(triggered(bool)), this, SLOT(changeStackedIndex(bool)));
+    connect(ui->actionS3, SIGNAL(triggered(bool)), this, SLOT(changeStackedIndex(bool)));//SLOT(toolBarS3Clicked(bool)));
+    connect(ui->actionAbout, SIGNAL(triggered(bool)), this, SLOT(changeStackedIndex(bool)));//SLOT(toolBarAboutClicked(bool)));
 }
 
-void MainWindow::toolBarS3Clicked( bool checked )
+void MainWindow::setPageIndex(int index)
 {
-    ui->stackedWidget->setCurrentIndex(0);
+    ui->stackedWidget->setCurrentIndex(index);
+    for (int i = 0; i < ui->stackedWidget->count(); i++)
+    {
+        QWidget* widget = ui->stackedWidget->widget(i);
+        if (i != index)
+        {
+            widget->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+        }
+    }
+    ui->stackedWidget->currentWidget()->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    ui->stackedWidget->currentWidget()->adjustSize();
+    resizeWithAnimation(sizeHint());
 }
 
-void MainWindow::toolBarAboutClicked(bool checked )
+void MainWindow::changeStackedIndex( bool checked)
 {
-    ui->stackedWidget->setCurrentIndex(1);
+    if (!checked)
+        return;
+    // Get all of the items and see which one is checked
+    for (QAction* action: toolBarGroup->actions())
+    {
+        if (action->isChecked())
+        {
+            if (action == ui->actionGeneral_Settings)
+                setPageIndex(0);
+            else if (action == ui->actionS3)
+                setPageIndex(1);
+            else if (ui->actionAbout)
+                setPageIndex(2);
+        }
+    }
 }
 
 //TODO: Add text input masks/validation (invalid chars, lengths, etc.)
--- mainwindow.h
+++ mainwindow.h
@@ -11,6 +11,7 @@ namespace Ui {
 class MainWindow;
 }
 
+class QPropertyAnimation;
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -18,6 +19,8 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+    void resizeWithAnimation( const QSize& size);
     
 private:
     Ui::MainWindow *ui;
@@ -26,12 +29,13 @@ private:
     std::shared_ptr<QSettings> appSettings;
     AWS::AWSCredentials userCredentials;
     std::unique_ptr<QActionGroup> toolBarGroup;
+    std::unique_ptr<QPropertyAnimation> animation; // used for changing the window size
 
     void setToolBarGroup( void );
+    void setPageIndex(int index);
 
 private slots:
-    void toolBarS3Clicked(bool checked );
-    void toolBarAboutClicked( bool checked );
+    void changeStackedIndex(bool checked);
 
     void lineBucketEditingFinished( void );
     void lineIdEditingFinished( void );
